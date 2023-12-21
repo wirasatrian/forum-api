63f2c278ccb94fab43dd099ccdc7d0aeaa008211
Wira Satria Negara <wirasatrian@gmail.com>
2023-12-21 09:44:52 +0800

    add hello world failed test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const users = require('../../Interfaces/http/api/users');
@@ -6,7 +7,6 @@ const authentications = require('../../Interfaces/http/api/authentications');
 const threads = require('../../Interfaces/http/api/threads');
 const comments = require('../../Interfaces/http/api/comments');
 const replies = require('../../Interfaces/http/api/replies');
-const Jwt = require('@hapi/jwt');
 
 const createServer = async (container) => {
   const server = Hapi.server({
